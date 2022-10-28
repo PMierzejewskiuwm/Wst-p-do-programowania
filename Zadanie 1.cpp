33e911c0ffb09d3fa005f9593a62753a6631919e
PMierzejewskiuwm <115549303+PMierzejewskiuwm@users.noreply.github.com>
2022-10-28 09:06:23 +0200

    Add files via upload

---
+++ Zadanie 1.cpp
@@ -0,0 +1,44 @@
+﻿// ConsoleApplication2.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    //Zadanie 1
+    int x, y, z, s;
+    cout << "Wpisz cene produktu ";
+    cin >> x;
+    cout << "Wpisz cene sprzedazy ";
+    cin >> y;
+    z = y - x;
+    s = x - y;
+    if (z > 0) {
+
+        cout << "Zysk wynosi: " << z;
+
+    }
+
+    else if (z < 0) {
+
+        cout << "Strata wynosi: " << s;
+    }
+
+
+
+
+}
+
+
+
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
