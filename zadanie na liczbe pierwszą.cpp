48980e7993b10e51529dc021189908f2f05aa3df
PMierzejewskiuwm <115549303+PMierzejewskiuwm@users.noreply.github.com>
2022-11-04 09:19:02 +0100

    Add files via upload

---
+++ zadanie na liczbe pierwszą.cpp
@@ -0,0 +1,42 @@
+﻿// ConsoleApplication7.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    bool isPrime = true;
+    cout << "Enter a number: " << endl;
+    cin >> n;
+    if (n <= 1) {
+
+        cout << n << "is not a prime number." << endl;
+
+    }
+    else {
+
+        for (int i = 2; i <= n / 2; i++) {
+
+            if (n % i == 0) {
+
+                isPrime = false;
+            }
+        }
+    }
+    if (isPrime) {
+
+        cout << n << " is a prime number.";
+    }
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
